5c1e873859fb5f1b079bbdcb16a88bad4788c0c0
Sanghack Lee <sanghack.lee@gmail.com>
2016-05-31 11:15:52 -0400

    yoyo

--- src/pyrcds/__robust.py
+++ src/pyrcds/__robust.py
@@ -1,24 +1,21 @@
 # Practical Learning Algorithm for RCM
 import collections
-import itertools
 import logging
 import typing
 from itertools import count, combinations
 from warnings import warn
 
-import networkx as nx
-
 from pyrcds.__rci import CITester
 from pyrcds.domain import RSchema, RSkeleton
-from pyrcds.model import PRCM, RVar, UndirectedRDep, canonical_rvars, GroundGraph, SymTriple
+from pyrcds.model import PRCM, RVar, UndirectedRDep, canonical_rvars, RCM
 from pyrcds.rcds import enumerate_rdeps
+
 
 # def group_by(xs, keyfunc):
 #     """Modified groupby from itertools with group objects as lists"""
 #     from itertools import groupby
 #     gb = groupby(sorted(xs, key=keyfunc), key=keyfunc)
 #     return ((k, list(g)) for k, g in gb)  # generator
-from pyrcds.utils import group_by
 
 
 def sound_rules(pcdg_xyz):
@@ -62,10 +59,10 @@ class PracticalLearner:
                 return True
         return False
 
-    def collider_ci_test(self):
-        return False
+    # def collider_ci_test(self):
+    #     return False
 
-    def phase_I(self) -> set:
+    def phase_I(self, truth: RCM=False, verbose=False, **kwargs) -> set:
         self.logger.info('phase I: started.')
         prcm, schema, ci_tester = self.prcm, self.schema, self.ci_tester
 
@@ -76,14 +73,22 @@ class PracticalLearner:
         for d in count():
             self.logger.info('phase I: checking depth: {}'.format(d))
             to_remove = set()
-            # TODO parallelize
             for udep in udeps_to_be_tested:
                 for dep in udep:
                     self.logger.info('phase I: checking: {}'.format(dep))
                     cause, effect = dep
                     if self.adjacency_ci_test(cause, effect, prcm.ne(effect), d):
+                        if truth:
+                            if cause in truth.adj(effect):
+                                if verbose:
+                                    print('False: {} _||_ {} | {}'.format(cause, effect, self.sepset[(cause, effect)]))
+                            else:
+                                if verbose:
+                                    print('False: {} _||_ {} | {}'.format(cause, effect, self.sepset[(cause, effect)]))
                         to_remove.add(udep)
                         break
+                else:
+                    pass
 
             # post clean up
             prcm.remove(to_remove)
@@ -95,59 +100,59 @@ class PracticalLearner:
         self.logger.info('phase I: finished.')
         return set(prcm.undirected_dependencies)
 
-    def phase_II(self, background_knowledge=None):
-        pcdg = self.prcm.class_dependency_graph
-        pgg = GroundGraph(self.prcm, self.skeleton)
-
-        if background_knowledge:
-            pcdg.orients(background_knowledge)
-
-        # find all unshielded triples
-        item_attr_uts = list(pgg.unshielded_triples())
-
-        def attrfy(symtriple_of_item_attribute):
-            (_, x), (_, y), (_, z) = symtriple_of_item_attribute
-            return SymTriple(x, y, z) if x < z else SymTriple(z, y, x)
-
-        # TODO address conflict?
-        # TODO parallelize
-        self.ori_results = {(x, y, z): self.collider_ci_test((x, y, z), item_attr_triples)
-                            for (x, y, z), item_attr_triples in group_by(item_attr_uts, attrfy)}
-
-        singletons = {}
-        for (x, y, z), result in self.ori_results.items():
-            if result.is_collider:
-                ppcdg = pcdg.copy()
-                ppcdg.orients(((x, y), (z, y)))
-                singletons[frozenset({(x, y, z)})] = (ppcdg, set(), result.score)
-            elif result.is_non_collider:
-                if x == z:
-                    ppcdg = pcdg.copy()
-                    ppcdg.orient(y, x)
-                    singletons[frozenset({(x, y, z)})] = (ppcdg, set(), result.score)
-                else:
-                    singletons[frozenset({(x, y, z)})] = (pcdg.copy(), {(x, y, z)}, result.score)
-
-        maximals = {1: singletons}
-        for size in itertools.count(2):
-            for m1, m2 in combinations(maximals[size - 1].keys(), 2):
-                if len(m1 & m2) == size - 2 and self.checkcheck(m1, m2, maximals[size - 1]):
-                    maximals[size].update({})
-
-            if not maximals[size]:
-                break
-
-        maximals.values()
-
-    def checkcheck(self, m1, m2, reference):
-        ppcdg1, nc1, score1 = reference[m1]
-        ppcdg2, nc2, score2 = reference[m2]
-        orients = ppcdg1.oriented() | ppcdg2.oriented()
-        if any((y, x) in orients for x, y in orients):
-            return False
-        dag = nx.DiGraph()
-        dag.add_edges_from(orients)
-        if not nx.is_directed_acyclic_graph(dag):
-            return False
-            # sound rules
-            # PDAG extensibility
+    # def phase_II(self, background_knowledge=None, truth=None,verbose=False, **kwargs):
+    #     pcdg = self.prcm.class_dependency_graph
+    #     pgg = GroundGraph(self.prcm, self.skeleton)
+    #
+    #     if background_knowledge:
+    #         pcdg.orients(background_knowledge)
+    #
+    #     # find all unshielded triples
+    #     item_attr_uts = list(pgg.unshielded_triples())
+    #
+    #     def attrfy(symtriple_of_item_attribute):
+    #         (_, x), (_, y), (_, z) = symtriple_of_item_attribute
+    #         return SymTriple(x, y, z) if x < z else SymTriple(z, y, x)
+    #
+    #     # TODO address conflict?
+    #     # TODO parallelize
+    #     self.ori_results = {(x, y, z): self.collider_ci_test((x, y, z), item_attr_triples)
+    #                         for (x, y, z), item_attr_triples in group_by(item_attr_uts, attrfy)}
+    #
+    #     singletons = {}
+    #     for (x, y, z), result in self.ori_results.items():
+    #         if result.is_collider:
+    #             ppcdg = pcdg.copy()
+    #             ppcdg.orients(((x, y), (z, y)))
+    #             singletons[frozenset({(x, y, z)})] = (ppcdg, set(), result.score)
+    #         elif result.is_non_collider:
+    #             if x == z:
+    #                 ppcdg = pcdg.copy()
+    #                 ppcdg.orient(y, x)
+    #                 singletons[frozenset({(x, y, z)})] = (ppcdg, set(), result.score)
+    #             else:
+    #                 singletons[frozenset({(x, y, z)})] = (pcdg.copy(), {(x, y, z)}, result.score)
+    #
+    #     maximals = {1: singletons}
+    #     for size in itertools.count(2):
+    #         for m1, m2 in combinations(maximals[size - 1].keys(), 2):
+    #             if len(m1 & m2) == size - 2 and self.checkcheck(m1, m2, maximals[size - 1]):
+    #                 maximals[size].update({})
+    #
+    #         if not maximals[size]:
+    #             break
+    #
+    #     maximals.values()
+    #
+    # def checkcheck(self, m1, m2, reference):
+    #     ppcdg1, nc1, score1 = reference[m1]
+    #     ppcdg2, nc2, score2 = reference[m2]
+    #     orients = ppcdg1.oriented() | ppcdg2.oriented()
+    #     if any((y, x) in orients for x, y in orients):
+    #         return False
+    #     dag = nx.DiGraph()
+    #     dag.add_edges_from(orients)
+    #     if not nx.is_directed_acyclic_graph(dag):
+    #         return False
+    #         # sound rules
+    #         # PDAG extensibility
--- src/tests/test_ci.py
+++ src/tests/test_ci.py
@@ -1,4 +1,3 @@
-import multiprocessing
 import unittest
 from itertools import combinations
 
@@ -34,40 +33,36 @@ class TestCI(unittest.TestCase):
 
     def test_ci_company_domain(self):
         schema, rcm = company_schema(), company_rcm()
-        skeleton = generate_skeleton(schema)
-
         functions = dict()
         effects = {RVar(RPath(rcm.schema.item_class_of(attr)), attr) for attr in rcm.schema.attrs}
 
-        for e in effects:
-            parameters = {cause: 1.0 for cause in rcm.pa(e)}
-            # with non-zero mean
-            functions[e] = linear_gaussian(parameters, average_agg(), normal_sampler(1, 0.3))
+        for _ in range(10):
+            skeleton = generate_skeleton(schema)
 
-        lg_rcm = ParamRCM(rcm.schema, rcm.directed_dependencies, functions)
+            for e in effects:
+                parameters = {cause: 1.0 for cause in rcm.pa(e)}
+                # with non-zero mean
+                functions[e] = linear_gaussian(parameters, average_agg(), normal_sampler(1, 0.5))
 
-        generate_values_for_skeleton(lg_rcm, skeleton)
+            lg_rcm = ParamRCM(rcm.schema, rcm.directed_dependencies, functions)
 
-        n_cpu = multiprocessing.cpu_count()
-        B = n_cpu
-        while B < 16:
-            B += n_cpu
+            generate_values_for_skeleton(lg_rcm, skeleton)
 
-        tester = SetKernelRCITester(skeleton, maxed=300, alpha=0.05, n_jobs=1, B=1)
-        print('degree: {}'.format(lg_rcm.degree))
-        for cond_size in range(lg_rcm.degree):
-            print('with cond size: {}'.format(cond_size))
-            for d0 in lg_rcm.directed_dependencies:
-                for d in ((d0, reversed(d0)) if cond_size > 0 else (d0,)):
-                    conds = lg_rcm.adj(d.effect) - {d.cause}
-                    for cond in combinations(conds, cond_size):
-                        print('testing {} _||_ {} | {}'.format(d.cause, d.effect, cond))
-                        ci_result = tester.ci_test(d.cause, d.effect, cond)
-                        if ci_result.ci:
-                            print("wrong:  {}: {}".format(d, ci_result))
-                        else:
-                            print("passed: {}: {}".format(d, ci_result))
-                    print()
+            tester = SetKernelRCITester(skeleton, alpha=0.025, n_jobs=-2, B=30, b=800, M=10000)
+            print('degree: {}'.format(lg_rcm.degree))
+            for cond_size in (0, 1):
+                print('with cond size: {}'.format(cond_size))
+                for d0 in lg_rcm.directed_dependencies:
+                    for d in ((d0, reversed(d0)) if cond_size > 0 else (d0,)):
+                        conds = lg_rcm.adj(d.effect) - {d.cause}
+                        for cond in combinations(conds, cond_size):
+                            print('testing {} _||_ {} | {}'.format(d.cause, d.effect, cond))
+                            ci_result = tester.ci_test(d.cause, d.effect, cond)
+                            if ci_result.ci:
+                                print("wrong:  {}: {}".format(d, ci_result.p))
+                            else:
+                                print("passed: {}: {}".format(d, ci_result.p))
+                        print()
 
 
 if __name__ == '__main__':
